80f8705d9c750f6948257098b622fdfad618ee83
Jerrylejer <81814979+Jerrylejer@users.noreply.github.com>
2023-02-20 11:03:15 +0100

    CREATION DU ROUTING

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { CustomizationComponent } from './pages/customization/customization.component';
+import { BasketComponent } from './pages/basket/basket.component';
+import { OrderConfirmationComponent } from './pages/order-confirmation/order-confirmation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    CustomizationComponent,
+    BasketComponent,
+    OrderConfirmationComponent
   ],
   imports: [
     BrowserModule,
