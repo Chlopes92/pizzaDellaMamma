768034f82907a48733bce3fe2ed05e4740b656dc
AssaNiang <mbayeou576@gmail.com>
2023-02-21 10:33:57 +0100

    debut d'affichage  de la page product

---
+++ src/app/services/products/products.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { IProduct, ProductCategoryType, PRODUCTS } from 'src/app/mocks/products.mock';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  constructor() { }
+
+  // retourne la liste de tous les produits
+  getProducts(): IProduct[] {
+    return PRODUCTS;
+  }
+  //retourn une categorie de produits
+  getCategoryProduct(category: ProductCategoryType): IProduct[] {
+    return PRODUCTS.filter(categoryProduct => categoryProduct.category === category)
+  }
+}
