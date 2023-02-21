0a577a32a3374890bfa466dcbc37846db725f52a
Jerrylejer <81814979+Jerrylejer@users.noreply.github.com>
2023-02-21 16:53:55 +0100

    AJOUT METHODES AU PRODUCTSERVICE _ A TESTER

--- src/app/components/ingredients-custom/ingredients-custom.component.ts
+++ src/app/components/ingredients-custom/ingredients-custom.component.ts
@@ -1,29 +1,30 @@
 import { Component } from '@angular/core';
 import { PRODUCTS, IProduct } from 'src/app/mocks/products.mock';
+import { ProductsService } from 'src/app/services/products/products.service';
 
 @Component({
   selector: 'app-ingredients-custom',
   templateUrl: './ingredients-custom.component.html',
-  styleUrls: ['./ingredients-custom.component.css']
+  styleUrls: ['./ingredients-custom.component.css'],
 })
 export class IngredientsCustomComponent {
   // Je mets en propriétés mon mocks products.mock.ts
   product!: IProduct;
   products: IProduct[] = PRODUCTS;
+  // Je récupère mon service où j'ai implémenté ma méthode pour récupérer la liste des ingrédients extras
+  constructor(public ingredients: ProductsService) {}
   // Mon compteur
   counter: number = 0;
- 
-ngOnInit() {
 
-}
+  ngOnInit() {}
 
-increment(){
-this.counter += 1;
-return this.counter;
-}
+  increment() {
+    this.counter += 1;
+    return this.counter;
+  }
 
-decrement() {
-  this.counter -= 1;
-  return this.counter;
-}
+  decrement() {
+    this.counter -= 1;
+    return this.counter;
+  }
 }
