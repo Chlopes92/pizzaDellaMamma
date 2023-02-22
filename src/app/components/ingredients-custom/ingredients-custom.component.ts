c9a7c12597ec3e8c6edffd23febd604114da0bd3
Jerrylejer <81814979+Jerrylejer@users.noreply.github.com>
2023-02-22 15:05:52 +0100

    AVANT INTERVANTION MANON

--- src/app/components/ingredients-custom/ingredients-custom.component.ts
+++ src/app/components/ingredients-custom/ingredients-custom.component.ts
@@ -13,18 +13,40 @@ export class IngredientsCustomComponent {
   products: IProduct[] = PRODUCTS;
   // Je récupère mon service où j'ai implémenté ma méthode pour récupérer la liste des ingrédients extras
   constructor(public ingredients: ProductsService) {}
-  // Mon compteur
-  counter: number = 0;
 
-  ngOnInit() {}
-
-  increment() {
-    this.counter += 1;
-    return this.counter;
+  limitDown: number = 1;
+  quantity: any = this.products[0].extras[1].quantity;
+  maxQuantity: any = this.products[0].extras[1].maxQuantity;
+  
+  ngOnInit() {
+  }
+  
+  increment(i: any) {
+    this.quantity = this.products[0].extras[i].quantity;
+    this.maxQuantity = this.products[0].extras[i].maxQuantity;
+    // Au clic je récupère les bonnes datas des extras
+    console.log(this.quantity)
+    console.log(this.maxQuantity)
+    // Ma condition
+    while(this.quantity < this.maxQuantity) {
+      this.quantity += 1;
+      return this.quantity;
+    }
+    return this.quantity = this.quantity;
   }
 
-  decrement() {
-    this.counter -= 1;
-    return this.counter;
+  decrement(i: any) {
+    this.quantity = this.products[0].extras[i].quantity;
+    this.maxQuantity = this.products[0].extras[i].maxQuantity;
+      // Au clic je récupère les bonnes datas des extras
+    console.log(this.quantity)
+    console.log(this.maxQuantity)
+    // Ma condition
+    while(this.quantity > this.limitDown) {
+      this.quantity -= 1;
+      // console.log(this.quantity)
+      return this.quantity;
+    }
+    return this.quantity = 0;
   }
 }
