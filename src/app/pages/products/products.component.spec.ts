32cd305816c74abb6e06ff7c31df25b772e22589
AssaNiang <mbayeou576@gmail.com>
2023-02-22 16:59:50 +0100

    j'ai creer une page product et sa route path:PRODUCTS ;
    git status
    g

---
+++ src/app/pages/products/products.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PRODUCTSComponent } from './products.component';
+
+describe('PRODUCTSComponent', () => {
+  let component: PRODUCTSComponent;
+  let fixture: ComponentFixture<PRODUCTSComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PRODUCTSComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PRODUCTSComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
