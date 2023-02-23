ead852c0287790ab1125e84c240d8f8875d4521b
Chlopes92 <christelle.lopes92@gmail.com>
2023-02-23 20:56:08 +0100

    bouton retour en arrière

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,12 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  
-  // itemTable = ['1'];
 
-  // addItem(newTable: string){
-  //   this.itemTable.push(newTable);
-  //   console.log('newTable :', this.itemTable);
-  // }
 
 }
