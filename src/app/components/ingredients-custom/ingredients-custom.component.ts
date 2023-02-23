fa0e5452713bebfea8e86b5bd675c5fd2af26be9
Jerrylejer <81814979+Jerrylejer@users.noreply.github.com>
2023-02-23 13:01:05 +0100

    POUR PUSH SUR DEV RECONFIGURE

--- src/app/components/ingredients-custom/ingredients-custom.component.ts
+++ src/app/components/ingredients-custom/ingredients-custom.component.ts
@@ -9,38 +9,67 @@ import { ProductsService } from 'src/app/services/products/products.service';
 })
 export class IngredientsCustomComponent {
   // Je mets en propriétés mon mocks products.mock.ts
-  // product!: IProduct;
-  // products: IProduct[] = PRODUCTS;
-
   product!: IProduct;
-  products: IProductsByCategory[] = PRODUCTS
+  products: IProductsByCategory[] = PRODUCTS;
+  limitDown: number = 0;
+  extraPrice: number = 0;
+  totalExtras: number = 0;
+  
   // Je récupère mon service où j'ai implémenté ma méthode pour récupérer la liste des ingrédients extras
   constructor(public ingredients: ProductsService) {}
 
-  limitDown: number = 0;
-
-  ngOnInit() {}
+  ngOnInit(i: any) {
+    this.totalAmount(i);
+    console.log(this.totalExtras)
+  }
 
   increment(i: any) {
     // Je modifie DIRECTEMENT la quantité en index de mon mock
-    // JE NE PASSE PAS par une variable ex. quantity = this.products[0].extras[i].quantity => qui sera commune à tous les compteurs et
-    // incrémentera donc tous les compteurs
+    // JE NE PASSE PAS par une variable ex. quantity = this.products[0].extras[i].quantity => qui sera commune à tous les compteurs et incrémentera donc tous les compteurs
     while (
       this.products[0].products[0].extras[i].quantity <
       this.products[0].products[0].extras[i].maxQuantity
     ) {
-      return (this.products[0].products[0].extras[i].quantity += 1);
+      return this.products[0].products[0].extras[i].quantity++;
     }
     return this.products[0].products[0].extras[i].maxQuantity;
   }
 
   decrement(i: any) {
     // Je modifie DIRECTEMENT la quantité en index de mon mock
-    // JE NE PASSE PAS par une variable ex. quantity = this.products[0].extras[i].quantity => qui sera commune à tous les compteurs et
-    // incrémentera donc tous les compteurs
+    // JE NE PASSE PAS par une variable ex. quantity = this.products[0].extras[i].quantity => qui sera commune à tous les compteurs et incrémentera donc tous les compteurs
     while (this.products[0].products[0].extras[i].quantity > this.limitDown) {
-      return (this.products[0].products[0].extras[i].quantity -= 1);
+      return (this.products[0].products[0].extras[i].quantity--);
     }
     return (this.products[0].products[0].extras[i].maxQuantity = this.limitDown);
+  }
+
+  // addToExtraPrice(i: any) {
+  //     this.extraPrice = this.products[0].products[0].extras[i].quantity * this.products[0].products[0].extras[i].additionalPrice;
+  //     this.totalExtras = this.totalExtras + this.extraPrice;
+  //     console.log(this.products[0].products[0].extras[i].quantity)
+  //     console.log(this.products[0].products[0].extras[i].additionalPrice)
+  //     console.log(this.extraPrice)
+  //     console.log(this.totalExtras)
+  // }
+
+  // deductFromExtraPrice(i: any) {
+  //   // this.extraPrice = this.products[0].products[0].extras[i].quantity * this.products[0].products[0].extras[i].additionalPrice;
+  //   this.totalExtras = this.totalExtras - this.extraPrice;
+
+  //   console.log(this.products[0].products[0].extras[i].quantity)
+  //   console.log(this.products[0].products[0].extras[i].additionalPrice)
+  //   console.log(this.extraPrice)
+  //   console.log(this.totalExtras)
+  // }
+
+  totalAmount(i: any){
+    this.totalExtras = this.products.reduce((accumulateur, selectedExtras)=>{
+      // Je récupère les quantités et les prix cliqués en console
+      console.log(selectedExtras.products[0].extras[i].quantity);
+      console.log(selectedExtras.products[0].extras[i].additionalPrice);
+      // Je retourne l'accumulateur + le produit de Q x P
+      return accumulateur += (selectedExtras.products[0].extras[i].quantity * selectedExtras.products[0].extras[i].additionalPrice);
+    }, 0);
   }
 }
